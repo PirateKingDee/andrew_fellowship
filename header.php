5f2491a2433184f0d941443b842163ba0f1c8fe4
PirateKingDee <andyliang@Andys-MacBook-Pro-3.local>
2017-01-15 19:19:00 -0800

    Added files

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+<header>
+	<h1>Andrew Fellowship</h1>
+	<p>put a quote here</p>
+	<nav>
+		<ul>
+			<a href=""><li>page1</li></a>
+			<a href=""><li>page2</li></a>
+			<a href=""><li>page3</li></a>
+		</ul>
+	</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">  <!tell borswer how our web format. what style we use.>
+	    <title>Andrew Fellowship</title>
+	    <link rel="stylesheet" href="css/normalize.css">
+	    <link rel="stylesheet" href="css/main.css">
+	    <link rel="stylesheet" href="css/responsive.css">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<?php include("header.php"); ?>
+		<div>
+			<h1>main content will be put here</h1>
+			<h2d>Insert a picture below</h2>
+		</div>
+
+		<footer>
+			<p>&COPY Andy Liang</p>
+
+		</footer>
+	</body>
+</html>
